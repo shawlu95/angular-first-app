b00c4994108121ad0acd5dbc02429f0eb5b5b3aa
Shaw Lu <shawlu95@gmail.com>
2022-08-21 16:17:24 -0700

    Data binding: string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,13 @@
 // decorator to mark this thing as a component
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-server", // html tag, must be unique
-  templateUrl: "./server.component.html"
+  selector: 'app-server', // html tag, must be unique
+  templateUrl: './server.component.html',
 })
 export class ServerComponent {
+  serverId: number = 1;
+  serverStatus: string = 'online';
 
+  getServerStatus = () => this.serverStatus;
 }
