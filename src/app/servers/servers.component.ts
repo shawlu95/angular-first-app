873938de4d4242d5827d8b6b2f564c8530f7cd58
Shaw Lu <shawlu95@gmail.com>
2022-08-21 16:29:14 -0700

    Event binding to button click

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus: string = 'No server created';
 
   constructor() {
     setTimeout(() => {
@@ -15,4 +16,8 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  onCreateServer(): void {
+    this.serverCreationStatus = 'Server was created';
+  }
 }
