ea154a2b6107d3b3dc8190c1f76d0a2268605f0f
Shaw Lu <shawlu95@gmail.com>
2022-08-21 17:00:11 -0700

    ngStyle & ngClass directive with property binding
    Color text green if server is online, otherwise red.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,10 +4,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server', // html tag, must be unique
   templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+      .offline {
+        color: yellow;
+      }
+    `,
+  ],
 })
 export class ServerComponent {
   serverId: number = 1;
   serverStatus: string = 'online';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus = () => this.serverStatus;
+
+  getColor = () => (this.serverStatus === 'online' ? 'green' : 'red');
 }
